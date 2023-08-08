371e64a0b2337c74e550912f0a8223cd83365106
RaphaelTrd <141614914+RaphaelTrd@users.noreply.github.com>
2023-08-08 22:39:52 +1200

    Ajout d'une liste de commandes a retenir

---
+++ usefull_commands.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# bonnes videos pour les bases
+https://www.youtube.com/watch?v=cQepf9fY6cE&list=PLS1QulWo1RIYmaxcEqw5JhK3b-6rgdWO_&index=1
+
+apt install 
+
+cd #acceder
+ls #lister
+ls -al #lister avec le detail des permissions
+chmod +x #ajouter le droit a l execution
+
+read -p 'username : ' user_var
+read -sp 'password : ' password_var #sneacky
+echo "Le username is $user_var" 
+
